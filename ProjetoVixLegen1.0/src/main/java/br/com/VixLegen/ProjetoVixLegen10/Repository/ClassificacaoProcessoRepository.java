d532a62dec039437264046009a5c9535d8a87fc9
Raphael <raphalima2204@gmail.com>
2026-09-25 11:17:15 -0300

    fix(model): centralize status na classificacao do processo

--- ProjetoVixLegen1.0/src/main/java/br/com/VixLegen/ProjetoVixLegen10/Repository/ClassificacaoProcessoRepository.java
+++ ProjetoVixLegen1.0/src/main/java/br/com/VixLegen/ProjetoVixLegen10/Repository/ClassificacaoProcessoRepository.java
@@ -1,8 +1,16 @@
 package br.com.VixLegen.ProjetoVixLegen10.Repository;
 
+import br.com.VixLegen.ProjetoVixLegen10.Enums.StatusProcesso;
 import br.com.VixLegen.ProjetoVixLegen10.Model.ClassificacaoProcesso;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ClassificacaoProcessoRepository
- extends JpaRepository<ClassificacaoProcesso, Long>
-{}
+        extends JpaRepository<ClassificacaoProcesso, Long> {
+
+    Optional<ClassificacaoProcesso> findByProcessoIdProcesso(Long idProcesso);
+
+    List<ClassificacaoProcesso> findByStatus(StatusProcesso status);
+}
