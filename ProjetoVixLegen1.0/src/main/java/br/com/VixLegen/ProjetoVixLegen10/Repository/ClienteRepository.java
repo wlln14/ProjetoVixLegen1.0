0251f4a408a777a10ce277ca67608d7ab3bcc06c
Raphael <raphalima2204@gmail.com>
2026-09-25 11:17:25 -0300

    fix(model): corrige consultas de ClienteRepository

--- ProjetoVixLegen1.0/src/main/java/br/com/VixLegen/ProjetoVixLegen10/Repository/ClienteRepository.java
+++ ProjetoVixLegen1.0/src/main/java/br/com/VixLegen/ProjetoVixLegen10/Repository/ClienteRepository.java
@@ -1,16 +1,19 @@
 package br.com.VixLegen.ProjetoVixLegen10.Repository;
 
 import br.com.VixLegen.ProjetoVixLegen10.Model.Cliente;
-import br.com.VixLegen.ProjetoVixLegen10.Model.ProcessoJuridico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
-    List<ProcessoJuridico> findByIdCliente(Long idCliente);
-
     boolean existsByCpf(String cpf);
+
+    boolean existsByCnpj(String cnpj);
+
+    boolean existsByCpfAndIdClienteNot(String cpf, Long idCliente);
+
+    boolean existsByCnpjAndIdClienteNot(String cnpj, Long idCliente);
 
     List<Cliente> findByNomeCompletoContainingIgnoreCase(String nome);
 }
